230cddc9b89bd90f5ac54e3f43ad43178e3d0db8
Allen Conlon <software@conlon.dev>
2026-05-21 21:11:29 -0400

    Add pinentry-curses and pinentry-qt to build script
    Added pinentry-curses and pinentry-qt to the package installation.

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y nix-core nix-daemon nix-legacy busybox util-linux
+dnf5 install -y nix-core nix-daemon nix-legacy busybox util-linux pinentry-curses pinentry-qt
 
 # Use a COPR Example:
 #
